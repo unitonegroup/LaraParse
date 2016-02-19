6312267f6ee75b61484d021c7260071949eee177
Mohammed S Shurrab <m.sh@unitone.ps>
2016-02-19 21:32:34 +0200

    add service provider to support routes

---
+++ src/LaraParseServiceProvider .php
@@ -0,0 +1,21 @@
+<?php
+
+namespace LaraParse;
+
+use Illuminate\Support\ServiceProvider;
+
+
+class LaraParseServiceProvider extends ServiceProvider
+{
+
+	public function register()
+    {
+		
+	}
+	
+	public function boot(){
+		if (! $this->app->routesAreCached()) {
+			require __DIR__.'/routes/routes.php';
+		}
+	}
+}
